c67739de52fc222e50265b7a9474e9ff79885d67
Damian Ubowski <sverda300@gmail.com>
2020-10-09 18:12:26 +0200

    Hide navbar at start page

--- js/main.js
+++ js/main.js
@@ -2,3 +2,38 @@ function pointTo(anchor)
 {
     window.location.href = anchor;
 }
+
+window.onload = (event) =>
+{
+    if (window.innerWidth < 768) 
+    {
+        return;
+    }
+
+    navbar = document.getElementsByClassName("navbar")[0];
+    navbar.style.display = "none";
+}
+
+$(window).scroll(() => 
+{
+    hideOnTop("navbar");
+});
+
+function hideOnTop(className)
+{
+    if (window.innerWidth < 768) 
+    {
+        return;
+    }
+
+    var scroll = $(window).scrollTop();
+    navbar = document.getElementsByClassName(className)[0];
+    if (scroll > window.innerHeight - (window.innerHeight * 0.5))
+    {
+        navbar.style.display = "block";
+    }
+    else
+    {
+        navbar.style.display = "none";
+    }
+}
